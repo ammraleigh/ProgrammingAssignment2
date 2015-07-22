0d537277bf69d4ae293db43564becd07d16076bd
ammraleigh <ammraleigh@gmail.com>
2015-07-22 16:23:28 -0400

    Update assignment methods

--- cachematrix.R
+++ cachematrix.R
@@ -3,10 +3,23 @@
 ## the data to be store (cached) locally using makeCacheMatrix and
 ## retrieve using cacheSolve.  
 
-## makeCacheMatrix checks if the contents have changed. If so the
-## contents are recalculated and cached locally
+## makeCacheMatrix checks if the matrix inverse operation has already
+## been performed and cached. If not the operation is performed and
+## the result is cached. If a prior cached result is found the cached 
+## result is returned.
 
 makeCacheMatrix <- function(x = matrix()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(solve) m <<- solve
+  getinverse <- function() m
+  list(set = set, get = get,
+       setinverse = setinverse,
+       getinverse = getinverse)
 
 }
 
@@ -17,6 +30,15 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
 
 makeVector <- function(x = numeric()) {
